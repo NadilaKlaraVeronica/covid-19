413b0877d0ca4d88c12a81f0ad0a8851afa48221
NadilaKlaraVeronica <63834665+NadilaKlaraVeronica@users.noreply.github.com>
2020-04-24 19:37:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PROFIL</title>
+
+</head>
+<body background="5.jpeg">
+<center>
+	
+<br>
+<h1><font color="white" size="7"> PROFIL </font></h1>
+	
+	<h2><img src="irene.jpeg" height="200" width="140"></h2>
+	<h3><font color="white"> Nama : Nadila Klara Veronica</font></h3>
+	<h3><font color="white">Kelas : X RPL 4</font></h3>
+	<h3><font color="white">No.absen : 27</font></h3>
+	<h3><font color="white">Jurusan : RPL</font></h3>
+	<h3><font color="white">Sekolah : SMK Telkom Malang</font></h3>
+	<br>
+	<h2><a class="nav-item nav-link" href="http://localhost/CORONA/data/login.php"><font color="red" size="6">LOGIN</font></a></h2>
+
+</body>
+</html>

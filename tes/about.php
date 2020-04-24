fc40c3e6d95563192228cc6e5729a48914b98027
NadilaKlaraVeronica <63834665+NadilaKlaraVeronica@users.noreply.github.com>
2020-04-24 19:35:58 +0700

    Create about.php

---
+++ tes/about.php
@@ -0,0 +1,48 @@
+<?php
+include '../aset/header.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ABOUT</title>
+
+</head>
+<body>
+	
+<center>
+	
+<br>
+<h1> COVID-19 </h1>
+	
+	<h2><img src="1.jpg"></h2>
+	<h4><font size="3">Virus Corona atau severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2) adalah virus yang menyerang sistem pernapasan. Penyakit karena infeksi virus ini disebut COVID-19. Virus Corona bisa menyebabkan gangguan ringan pada sistem pernapasan, infeksi paru-paru yang berat, hingga kematian.</font></h4>
+<br>
+<br>
+	
+	<h2>Gejala Virus Corona</h2>
+	<h2><img src="2.jpg"></h2>
+	<h4><font size="3">Gejala awal infeksi virus Corona atau COVID-19 bisa menyerupai gejala flu, yaitu demam, pilek, batuk kering, sakit tenggorokan, dan sakit kepala. Setelah itu, gejala dapat hilang dan sembuh atau malah memberat. Penderita dengan gejala yang berat bisa mengalami demam tinggi, batuk berdahak bahkan berdarah, sesak napas, dan nyeri dada. Gejala-gejala tersebut muncul ketika tubuh bereaksi melawan virus Corona.</font></h4>
+
+<h4><font size="3">Secara umum, ada 3 gejala umum yang bisa menandakan seseorang terinfeksi virus Corona, yaitu:,</font></h4>
+
+<h4><font size="3">1. Demam (suhu tubuh di atas 38 derajat Celsius)</font></h4>
+<h4><font size="3">2. Batuk</font></h4>
+<h4><font size="3">3. Sesak napas</font></h4>
+<h4><font size="3">Gejala-gejala COVID-19 ini umumnya muncul dalam waktu 2 hari sampai 2 minggu setelah penderita terpapar virus Corona.</font></h4>
+<br>
+<br>
+
+<h2>Penyebab Virus Corona</h2>
+<h2><img src="3.png"></h2>
+<h4><font size="3">Seseorang dapat tertular COVID-19 melalui berbagai cara, yaitu:</font></h4>
+<h4><font size="3">1. Tidak sengaja menghirup percikan ludah (droplet) yang keluar saat penderita COVID-19 batuk atau bersin</font></h4>
+<h4><font size="3">2. Memegang mulut atau hidung tanpa mencuci tangan terlebih dulu setelah menyentuh benda yang terkena cipratan ludah penderita COVID-19</font></h4>
+<h4><font size="3">3. Kontak jarak dekat dengan penderita COVID-19</font></h4>
+<br>
+<h4><font size="6">Informasi lebih lanjut</font></h3>
+<h2><a href="https://www.alodokter.com/virus-corona"><font size="7">KLIK DISINI</font></a></h2>
+
+</body>
+</html>

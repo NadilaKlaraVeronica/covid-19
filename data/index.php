6ebf0ae8446308f0dde0c7b112821bf953504301
NadilaKlaraVeronica <63834665+NadilaKlaraVeronica@users.noreply.github.com>
2020-04-24 12:13:19 +0700

    Create index.php

---
+++ data/index.php
@@ -0,0 +1,73 @@
+<?php
+
+include '../koneksi.php';
+
+$sql = "SELECT * FROM data_odp ORDER BY nama_odp";
+
+$res = mysqli_query($kon, $sql);
+
+$index = array();
+
+while ($data = mysqli_fetch_assoc($res)){
+	$index[] = $data;
+}?>
+
+<?php
+include '../aset/header.php';
+?>
+
+<div class="container">
+  <div class="row mt-4">
+  	<div class="col-md">
+  </div>
+</div>
+</div>
+  	 
+<div class="card">
+	<div class="card-header">
+		<h2 class="card-title"><i class="fas fa-book"></i>DASHBOARD</h2>
+<br>
+			 
+	</div>
+	<div class="card-body">
+		
+<table class="table table-striped">
+	<thead>
+		<tr>
+			<th scope="col">#</th>
+			<th scope="col">Nama ODP</th>
+			<th scope="col">Umur ODP</th>
+			<th scope="col">Alamat ODP</th>
+			<th scope="col">Suhu Tubuh ODP</th>
+			
+		</tr>
+	</thead>
+	<?php
+	  $id = 1;
+	  foreach ($index as $p ) { ?>
+	  	<tr>
+	  		<th scope="row"><?= $id ?></th>
+	  		<td><?= $p['nama_odp'] ?></th>
+	  		<td><?= $p['umur_odp'] ?></th>
+	  		<td><?= $p['alamat_odp'] ?></th>
+	  		<td><?= $p['suhu_odp'] ?></th>
+	  		
+			<td>
+	  			<a href="detail.php?id_odp=<?= $p['id_odp']; ?>" class="badge badge-success">Detail</a>
+	  			<a href="edit.php?id_odp=<?= $p['id_odp']; ?>" class="badge badge-warning">Edit</a>
+	  			<a href="hapus.php?id_odp=<?= $p['id_odp']; ?>" class="badge badge-danger">Hapus</a>
+	  		</td>
+	  	</tr>
+	  	<?php
+	  	$id++;
+
+	  } ?>
+
+</table>
+	</div>
+</div>  	 
+  
+  
+<?php
+include '../aset/footer.php';
+?>
